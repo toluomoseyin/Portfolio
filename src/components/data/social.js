df68ec7f39d259e3af348f08351a54e72e44a8b6
tolu omoseyin <toluomoseyin01@gmail.com>
2021-08-05 19:42:43 +0100

    social media

--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,31 +1,31 @@
 export const SocialData = [
   {
-    patform: "Instagram",
-    link: "",
+    platform: "Instagram",
+    link: "https://www.instagram.com/toluaaron/",
     icon: require("../../assets/social/instagram.png").default,
   },
   {
-    patform: "Facebook",
-    link: "",
+    platform: "Facebook",
+    link: "https://www.facebook.com/profile.php?id=100008976947890",
     icon: require("../../assets/social/facebook.png").default,
   },
   {
-    patform: "WhatsApp",
-    link: "",
+    platform: "WhatsApp",
+    link: "https://wa.link/vbzqr2",
     icon: require("../../assets/social/whatsapp.png").default,
   },
   {
-    patform: "LinkedIn",
-    link: "",
+    platform: "LinkedIn",
+    link: "https://www.linkedin.com/in/tolu-omoseyin-b08474215/",
     icon: require("../../assets/social/linkedin.png").default,
   },
   {
-    patform: "Github",
-    link: "",
+    platform: "Github",
+    link: "https://github.com/toluomoseyin",
     icon: require("../../assets/social/github.png").default,
   },
   {
-    patform: "Twitter",
+    platform: "Twitter",
     link: "",
     icon: require("../../assets/social/twitter.png").default,
   },
